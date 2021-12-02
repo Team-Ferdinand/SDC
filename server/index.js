ade1899e1f11f8617a93f22f5c5fdac27811ba33
Matthew Seagren <mattseagren@gmail.com>
2021-12-02 15:36:48 -0600

    Completed the Master Server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const axios = require('axios');
 
 app.use(express.json());
 
@@ -9,29 +10,74 @@ app.listen(port, () => {
   console.log(`App listening at Port:${port}`);
 });
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Master Server');
+})
+
 //Products
 app.get('/products', (req, res) => {
-  res.redirect('http://ec2-18-224-153-242.us-east-2.compute.amazonaws.com:3000')
-    .catch((err) => {
-      console.log(err);
-    });
+  res.redirect('http://ec2-18-224-153-242.us-east-2.compute.amazonaws.com:80/products');
 });
 
 app.get('/products/*', (req, res) => {
-  res.redirect('http://ec2-18-224-153-242.us-east-2.compute.amazonaws.com:3000')
-    .catch((err) => {
-      console.log(err);
-    });
+  res.redirect(`http://ec2-18-224-153-242.us-east-2.compute.amazonaws.com:80${req.url}`);
 });
 
 
 // Reviews
-app.get('/products', (req, res) => {
+const reviewAddress = 'http://ec2-18-224-94-161.us-east-2.compute.amazonaws.com:80';
+app.get('/reviews', (req, res) => {
+  res.redirect(`${reviewAddress}${req.url}`);
+});
 
+app.get('/reviews/meta', (req, res) => {
+  res.redirect(`${reviewAddress}${req.url}`);
+});
+
+app.post('/reviews', (req, res) => {
+  axios.post(`${reviewAddress}${req.url}`, req.body)
+    .then((response) => {
+      res.status(201).json(response.data)
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    })
+});
+
+app.put('/reviews/*', (req, res) => {
+  axios.put(`${reviewAddress}${req.url}`)
+    .then((response) => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    })
 });
 
 
 // Q&A
-app.get('/products', (req, res) => {
+const qaAddress = 'http://ec2-3-144-186-10.us-east-2.compute.amazonaws.com:80';
+app.get('/qa/*', (req, res) => {
+  res.redirect(`${qaAddress}${req.url}`);
+});
 
+app.post('/qa/*', (req, res) => {
+  axios.post(`${qaAddress}${req.url}`, req.body)
+    .then((response) => {
+      console.log(response.data)
+      res.status(201).json(response.data);
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    })
+});
+
+app.put('/qa/*', (req, res) => {
+  axios.put(`${qaAddress}${req.url}`)
+    .then((response) => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    })
 });
